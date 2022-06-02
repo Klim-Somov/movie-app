b1312aa82ed3a301981ac0a74dc988ccd85590f0
Klim <klim_somov@mail.ru>
2022-06-02 21:28:00 +0300

    create movie reducer

--- src/redux/features/moveSlice.js
+++ src/redux/features/moveSlice.js
@@ -1,12 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const movieSlise = createSlice({
-    name: "movie",
-    initialState: {
-        moviesList: [],
-        movie: {},
-
+  name: "movie",
+  initialState: {
+    moviesList: [],
+    movie: {},
+  },
+  reducer: {
+    getMovies(name) {
+      return name;
     },
-})
+    setMovies: (state, { payload }) => {
+      state.moviesList = payload;
+    },
+  },
+});
 
-export default movieSlise.reducer
+export const { getMovies, setMovies } = movieSlise.actions;
+
+export default movieSlise.reducer;
